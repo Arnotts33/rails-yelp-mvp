f8cde9748087237b8c94f35d4d72a44160f56fbe
Arnaud Lahaut <arnaudlht@gmail.com>
2024-01-24 16:07:17 +0100

    challenge done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Clean database"
+Restaurant.destroy_all
+
+puts "Creating seeds"
+
+miles = {name: "Miles", address: "33, rue du Cancera 33000 Bordeaux", phone_number: "0556811824", category: "french"}
+chez_ba = {name: "Chez Ba", address: "15, rue Leyteire 33000 Bordeaux", phone_number: "0556784839", category: "french"}
+massa = {name: "Massa", address: "36, rue des Ayres 33000 Bordeaux", phone_number: "0556987654", category: "italian"}
+la_frite = {name: "La Frite", address: "4, rue de la frite 33000 Bordeaux", phone_number: "0556236578", category: "belgian"}
+bao = {name: "Bao", address: "56, rue de la brioche 33000 Bordeaux", phone_number: "0556090909", category: "chinese"}
+
+[miles, chez_ba, massa, la_frite, bao].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Seeds created!"
